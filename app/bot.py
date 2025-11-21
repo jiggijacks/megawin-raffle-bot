ec1dbb2aa5e6b5f1d7c9ef65a25acb9cbb5da445
jiggijacks <114829766+jiggijacks@users.noreply.github.com>
2025-11-21 02:08:21 +0100

    Fix aiogram version

--- app/bot.py
+++ app/bot.py
@@ -1,6 +1,6 @@
 import logging
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode
+from aiogram.enums import ParseMode
 from aiogram.utils import executor
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from .config import BOT_TOKEN
