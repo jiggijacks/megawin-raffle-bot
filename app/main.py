2af34f599be0e96bfa29860b2c3f1a3cf49e41c4
jiggijacks <114829766+jiggijacks@users.noreply.github.com>
2025-11-29 03:30:35 +0100

    Fix webhook

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from aiogram import Bot, Dispatcher
 from aiogram.types import Update
+
 import os
 
 from app.bot import bot, dp
@@ -25,12 +26,8 @@ async def on_shutdown():
     await bot.delete_webhook()
 
 
-@app.post("/webhook/telegram")
+@app.post("/webhook")
 async def telegram_webhook(request: Request):
-    """
-    Aiogram 3 webhook endpoint
-    """
     data = await request.json()
-    update = Update(**data)
-    await dp.feed_update(bot, update)
+    await dp.feed_update(bot, data)
     return {"status": "ok"}
